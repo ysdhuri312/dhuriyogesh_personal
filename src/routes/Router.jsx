d209fd7021cff2a8b2d8b747494916cbb1a55219
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-23 19:44:20 +0530

    Setup router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,35 @@
 /** @format */
 
-const Router = () => {
-  return <>Router</>;
-};
-export default Router;
+import { createBrowserRouter } from 'react-router';
+import Root from '../layouts/Root';
+import Home from '../pages/Home';
+import Blog from '../pages/Blog';
+import BlogPost from '../pages/BlogPost';
+import Photos from '../pages/Photos';
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: '/blog',
+        element: <Blog />,
+      },
+      {
+        path: '/blog/:slug',
+        element: <BlogPost />,
+      },
+      {
+        path: '/photos',
+        element: <Photos />,
+      },
+    ],
+  },
+]);
+
+export default router;
