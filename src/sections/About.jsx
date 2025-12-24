123c3f32d77cb59264e44dd44dc61639a38b27d5
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-24 20:41:51 +0530

    Complete about me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,35 @@
 /** @format */
 
+import about from '../assets/img/hero-2.png';
+
 const About = () => {
-  return <section>About</section>;
+  return (
+    <section
+      id='about-me'
+      className='container mx-auto px-15 flex items-center gap-12 py-2.5 mb-20'
+    >
+      <div className='w-[40vw]'>
+        <img src={about} alt='hero-image' />
+      </div>
+      <div className='w-[60vw]'>
+        <h4 className='tracking-[7px] opacity-50 font-bold mb-2.5'>ABOUT ME</h4>
+        <h1 className='text-5xl font-black mb-6'>
+          I&#39;m here to help your next project !
+        </h1>
+        <p className='text-[17px] font-medium mb-9 pr-24'>
+          Hi there, My name is Yogesh Dhuri and I&#39;m Full stack web developer
+          having specialization in backend & Front web development. I have
+          backend skills in Node.js, Expess.js. I also have frontend skills in
+          JavaScript, react.js, Web components like css, Bootstraps.
+        </p>
+        <a
+          href='/#contact-me'
+          className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:opacity-90'
+        >
+          Hire Me
+        </a>
+      </div>
+    </section>
+  );
 };
 export default About;
