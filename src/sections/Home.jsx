9c753525fe7deca1dea2ecf84c727125f5e0f384
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-24 20:42:22 +0530

    Update paragraph text size

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -16,14 +16,14 @@ const Home = () => {
         <h1 className='text-5xl font-black mb-6'>
           Hello, I&#39;m Yogesh Dhuri !
         </h1>
-        <p className='font-medium mb-9 pr-24'>
+        <p className='font-medium mb-9 pr-24 text-[17px]'>
           I want transitioning into Junior Web Developer with hands-on
           experience in JavaScript, Node.js, and React. Seeking an entry-level
           software development role to apply my problem-solving skills and
           real-world business understanding in building scalable applications.
         </p>
         <a
-          href='#contact-me'
+          href='/#contact-me'
           className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:opacity-90'
         >
           Notify Me
