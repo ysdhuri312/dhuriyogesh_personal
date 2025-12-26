fb6c621ceb53bd18813b60e5d55c844c779472fc
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-26 11:54:47 +0530

    Add icons and export from single file

---
+++ src/assets/index.js
@@ -0,0 +1,42 @@
+/** @format */
+import bootstrap from './icons/bootstrap.png';
+import cpp from './icons/cpp.png';
+import css3 from './icons/css3.png';
+import docker from './icons/docker.png';
+import express from './icons/express-js.png';
+import github from './icons/github.png';
+import graphql from './icons/graphql.png';
+import html5 from './icons/html-5.png';
+import next from './icons/nextjs.png';
+import javascript from './icons/javascript.png';
+import jquery from './icons/jquery.png';
+import mongodb from './icons/mongodb.png';
+import node from './icons/node-js.png';
+import postgresql from './icons/postgresql.png';
+import python from './icons/python.png';
+import react from './icons/react.png';
+import sass from './icons/sass.png';
+import tailwind from './icons/tailwindcss.png';
+import typescript from './icons/typescript.png';
+
+export const icon = {
+  bootstrap,
+  cpp,
+  css3,
+  docker,
+  express,
+  github,
+  graphql,
+  html5,
+  javascript,
+  jquery,
+  mongodb,
+  next,
+  node,
+  postgresql,
+  python,
+  react,
+  sass,
+  tailwind,
+  typescript,
+};
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,6 +1,111 @@
 /** @format */
 
 const Skills = () => {
-  return <section>Skills</section>;
+  return (
+    <section>
+      {' '}
+      <h3>I&#39;m Specialized In</h3>
+      <div id='skills'>
+        <img class='icon' src='assets/icons/html-5.png' alt='html5' />
+        <img class='icon' src='assets/icons/css3.png' alt='css3' />
+        <img class='icon' src='assets/icons/javascript.png' alt='javascript' />
+        <img class='icon' src='assets/icons/bootstrap.png' alt='bootstrap' />
+        <img class='icon' src='assets/icons/react.png' alt='react' />
+        <img
+          class='icon'
+          src='assets/icons/tailwindcss.png'
+          alt='tailwindcss'
+        />
+        <img class='icon' src='assets/icons/node-js.png' alt='node-js' />
+        <img class='icon' src='assets/icons/express-js.png' alt='' />
+        <img class='icon' src='assets/icons/mongodb.png' alt='mongodb' />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/graphql.png'
+          alt='graphql'
+        />
+        <img class='icon' src='assets/icons/typescript.png' alt='typescript' />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/nextjs.png'
+          alt='nextjs'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/postgresql.png'
+          alt='postgresql'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/docker.png'
+          alt='docker'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/python.png'
+          alt='python'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/cpp.png'
+          alt='cpp'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/mongodb.png'
+          alt='mongodb'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/graphql.png'
+          alt='graphql'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/typescript.png'
+          alt='typescript'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/nextjs.png'
+          alt='nextjs'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/postgresql.png'
+          alt='postgresql'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/docker.png'
+          alt='docker'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/nextjs.png'
+          alt='nextjs'
+        />
+        <img
+          class='icon inprogress'
+          class='inprogress'
+          src='assets/icons/nextjs.png'
+          alt='nextjs'
+        />
+      </div>
+    </section>
+  );
 };
 export default Skills;
