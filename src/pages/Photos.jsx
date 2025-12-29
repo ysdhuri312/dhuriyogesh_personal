d1fb5d23700a47367eb1451c2e34b19f90c21bf5
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-29 12:01:35 +0530

    Add under construction page in photos

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,6 +1,46 @@
 /** @format */
 
+import UnderConstruction from '../assets/icons/under-construction.gif';
+
 const Photos = () => {
-  return <>Photos</>;
+  return (
+    <section
+      style={{
+        color: '#444',
+        margin: 0,
+        font: 'normal 14px/20px Arial, Helvetica, sans-serif',
+        height: '100%',
+        backgroundColor: '#fff',
+      }}
+    >
+      <div style={{ height: '100vh', minHeight: '100%' }}>
+        <div
+          style={{
+            textAlign: 'center',
+            width: '800px',
+            marginLeft: '-400px',
+            position: 'absolute',
+            top: '30%',
+            left: '50%',
+          }}
+        >
+          <img
+            src={UnderConstruction}
+            style={{
+              display: 'inline',
+              textAlign: 'center',
+              height: '150px',
+              width: '150px',
+            }}
+          />
+
+          <h2 style={{ fontSize: '30px' }}>Coming Soon</h2>
+          <p style={{ marginTop: '10px' }}>
+            Our website is under construction!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Photos;
