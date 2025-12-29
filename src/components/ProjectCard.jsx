8a5bcd0530b2310314d0bc2ade01610cdc7e50fe
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-29 13:22:15 +0530

    Complete blog list page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,36 @@
 /** @format */
 
+import placeholder from '../assets/icons/placeholder-image.png';
+
 const ProjectCard = () => {
-  return <>ProjectCard</>;
+  return (
+    <article className='border border-[#E1B6B6] bg-[#D9D9D93a] my-4 flex'>
+      <figure>
+        <img
+          src={placeholder}
+          alt='Raspberry Pi Home Automation'
+          className='block w-80 h-45 object-cover'
+        />
+      </figure>
+      <div className='p-2.5'>
+        <h4 className='font-bold hover:underline tracking-wide'>
+          <a href='/pages/blog.html'>
+            Raspberry Pi Home Automation with Google Assistant
+            integration — Part 3 (Assistant integration)
+          </a>
+        </h4>
+        <div class='blog__meta' className='opacity-50'>
+          <time datetime='2018-11-03'>Nov 3, 2018</time>
+          <span> • </span>
+          <span class='blog-reading__time'>6 min read</span>
+        </div>
+        <p className='font-light'>
+          So up till now you have been able to control the switches with the
+          React based frontend. Time to take things up a little notch. We’ll be
+          integrating Google Assistant...
+        </p>
+      </div>
+    </article>
+  );
 };
 export default ProjectCard;
