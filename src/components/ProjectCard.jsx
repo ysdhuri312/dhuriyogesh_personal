48db5e997f222dc2dcb3541f11fab880e0b28970
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-31 14:59:27 +0530

    Refactor repeatative code

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,44 @@
+/** @format */
+
+import portfolio from '../assets/thumbnails/portfolio.png';
+import yelpCamp from '../assets/thumbnails/yelp-camp.png';
+
+const ProjectCard = ({ projects }) => {
+  return (
+    <ul className='flex justify-center w-full h-96 gap-2 flex-wrap p-0'>
+      {projects.map((project, index) => {
+        return (
+          <li key={index} className='w-1/4 h-auto flex flex-col shadow-lg'>
+            <img
+              src={project.image}
+              className='w-full h-[50%] object-cover shadow-md'
+            />
+            <div className='m-2.5 h-auto flex flex-col justify-around'>
+              <h4 className='mt-2.5 text-md font-heading'>{project.name}</h4>
+              <p className='mt-2.5 text-sm'>{project.description}</p>
+              <div className='mt-4 mb-1'>
+                <a
+                  href={project.live_app_link}
+                  alt='live-link'
+                  className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:bg-[#d94b2c]'
+                  target='_blank'
+                >
+                  Live
+                </a>
+                <a
+                  href={project.source_code_link}
+                  alt='github-link'
+                  className='text-sm bg-primary px-3.5 py-1.5 text-amber-50 hover:bg-[#d94b2c]'
+                  target='_blank'
+                >
+                  Github
+                </a>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default ProjectCard;
--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -1,6 +1,7 @@
 /** @format */
-import portfolio from '../assets/thumbnails/portfolio.png';
-import yelpCamp from '../assets/thumbnails/yelp-camp.png';
+
+import ProjectCard from '../components/ProjectCard';
+import { projects } from '../constants/index';
 
 const Project = () => {
   return (
@@ -10,105 +11,7 @@ const Project = () => {
     >
       {' '}
       <h3 className='text-4xl font-heading text-center'>Featured My Works</h3>
-      <ul className='flex justify-center w-full h-96 gap-2 flex-wrap p-0'>
-        <li className='w-1/4 h-auto flex flex-col shadow-lg'>
-          <img
-            src={portfolio}
-            className='w-full h-[50%] object-cover shadow-md'
-          />
-          <div className='m-2.5 h-auto flex flex-col justify-around'>
-            <h4 className='mt-2.5 text-md font-heading'>Portfolio Website</h4>
-            <p className='mt-2.5 text-sm'>
-              My personal website - built with HTML, CSS, javascript and Deploy
-              on GitHub Pages.
-            </p>
-            <div className='mt-4 mb-1'>
-              <a
-                href='https://ysdhuri312.github.io/dhuriyogesh.github.io/'
-                alt='live-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:bg-[#d94b2c]'
-                target='_blank'
-              >
-                Live
-              </a>
-              <a
-                href='https://github.com/ysdhuri312/portfolio-app'
-                alt='github-link'
-                className='text-sm bg-primary px-3.5 py-1.5 text-amber-50 hover:bg-[#d94b2c]'
-                target='_blank'
-              >
-                Github
-              </a>
-            </div>
-          </div>
-        </li>
-        <li className='w-1/4 h-auto flex flex-col shadow-lg'>
-          <img
-            src={yelpCamp}
-            className='w-full h-[50%] object-cover shadow-md'
-          />
-          <div className='m-2.5 h-auto flex flex-col justify-around'>
-            <h4 className='mt-2 text-md font-heading'>Yelp Camp</h4>
-            <p className='mt-2 text-sm'>
-              A full-stack CRUD application to find nearest campground and if
-              you login then can add campground with images and description. It
-              build with Bootstrap, EJS, Express.js, MongoDB and deploy on
-              Render.
-            </p>
-            <div className='mt-4 mb-1'>
-              <a
-                href='https://yelp-camp-i3wi.onrender.com/'
-                alt='live-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Live
-              </a>
-              <a
-                href='https://github.com/ysdhuri312/yelp-camp.git'
-                alt='github-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Github
-              </a>
-            </div>
-          </div>
-        </li>
-        <li className='w-1/4 h-auto flex flex-col shadow-lg'>
-          <img
-            src={yelpCamp}
-            className='w-full h-[50%] object-cover shadow-md'
-          />
-          <div className='m-2.5 h-auto flex flex-col justify-around'>
-            <h4 className='mt-2 font-heading text-md'>Yelp Camp</h4>
-            <p className='mt-2 text-sm'>
-              A full-stack CRUD application to find nearest campground and if
-              you login then can add campground with images and description. It
-              build with Bootstrap, EJS, Express.js, MongoDB and deploy on
-              Render.
-            </p>
-            <div className='mt-4 mb-1'>
-              <a
-                href='https://yelp-camp-i3wi.onrender.com/'
-                alt='live-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Live
-              </a>
-              <a
-                href='https://github.com/ysdhuri312/yelp-camp.git'
-                alt='github-link'
-                className='text-sm mr-2 bg-primary px-3.5 py-1.5 text-amber-50 hover:opacity-90'
-                target='_blank'
-              >
-                Github
-              </a>
-            </div>
-          </div>
-        </li>
-      </ul>
+      <ProjectCard projects={projects} />
     </section>
   );
 };
