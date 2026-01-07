6d9f62262af86daefd97f9f63f3e69ee578ce7c5
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-07 15:46:00 +0530

    Complete mobile responsive about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,30 +6,32 @@ const About = () => {
   return (
     <section
       id='about-me'
-      className='container flex items-center gap-12 py-2.5 mb-20'
+      className='container flex flex-col-reverse sm:flex-row items-center gap-12 py-2.5 mb-20'
     >
-      <div className='w-[40vw]'>
-        <img src={about} alt='hero-image' />
+      <div className='items-center sm:w-[50vw]'>
+        <img src={about} alt='hero-image' className='sm:size-70 lg:size-90' />
       </div>
-      <div className='w-[60vw]'>
-        <h4 className='tracking-[7px] opacity-50 font-black mb-2.5'>
+      <div className='items-center sm:w-[50vw] xs:my-5'>
+        <h4 className='xs:text-center sm:text-left tracking-[7px] opacity-50 font-black mb-2.5'>
           ABOUT ME
         </h4>
-        <h1 className='text-5xl mb-6 font-heading'>
+        <h1 className='xs:text-center sm:text-left text-4xl mb-6 font-heading lg:text-5xl'>
           I&#39;m here to help your next project !
         </h1>
-        <p className='text-[17px] font-medium mb-9 pr-24'>
+        <p className='xs:text-center sm:text-left text-[17px] font-medium mb-9 lg:pr-24'>
           Hi there, My name is Yogesh Dhuri and I&#39;m Full stack web developer
           having specialization in backend & Front web development. I have
           backend skills in Node.js, Expess.js. I also have frontend skills in
           JavaScript, react.js, Web components like css, Bootstraps.
         </p>
-        <a
-          href='/#contact-me'
-          className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c]'
-        >
-          Hire Me
-        </a>
+        <div className='xs:text-center sm:text-left'>
+          <a
+            href='/#contact-me'
+            className='text-sm font-medium mr-2 bg-primary px-5 py-3 text-amber-50 hover:bg-[#d94b2c] '
+          >
+            Hire Me
+          </a>
+        </div>
       </div>
     </section>
   );
