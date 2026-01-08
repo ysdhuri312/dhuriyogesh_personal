73928789d519c0e3374b01108b5f415394af81b5
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-08 15:03:41 +0530

    Contact form submission process done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,31 +1,47 @@
 /** @format */
 
+import { useState } from 'react';
+import { data } from 'react-router';
+
 const Contact = () => {
+  const [result, setResult] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult('Sending....');
+    const formData = new FormData(event.target);
+    formData.append('access_key', 'cb2bd1ab-4a2a-4b30-ad1f-d1da81802f89');
+    console.log(formData);
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      setResult('Form Submitted Successfully...');
+      event.target.reset();
+    } else {
+      setResult('Error!!!');
+    }
+  };
+
   return (
     <section id='contact' className='container py-2.5'>
       <h2 className='text-4xl font-black text-center mb-2.5 font-heading '>
         Get In Touch
       </h2>
       <div className='flex flex-col md:flex-row md:justify-center justify-center gap-7 p-5'>
-        <div className='p-5 border rounded-xs border-[#f5bcbc] xs:w-full sm:w-125'>
-          <form
-            action='https://api.web3forms.com/submit'
-            method='POST'
-            id='form'
-          >
-            <input
-              type='hidden'
-              name='access_key'
-              value='cb2bd1ab-4a2a-4b30-ad1f-d1da81802f89'
-            />
-
+        <div className='p-4 border rounded-xs border-[#f5bcbc] xs:w-full sm:w-125'>
+          <form onSubmit={onSubmit}>
             <label htmlFor='name' className='block font-medium mt-2.5 mb-2'>
               Name
               <input
                 type='text'
                 id='name'
                 name='name'
-                placeholder=''
+                required
                 className='w-full p-2.5 rounded-xs border border-[#f5bcbc] outline-none mb-2.5 text-sm'
               />
             </label>
@@ -35,20 +51,17 @@ const Contact = () => {
                 type='email'
                 id='email'
                 name='email'
-                placeholder=''
+                required
                 className='w-full p-2.5 rounded-xs border border-[#f5bcbc] outline-none mb-2.5 text-sm'
               />
             </label>
-
-            {/* <label for="subject">Subject</label>
-                  <input type="text" id="subject" placeholder="" /> */}
 
             <label htmlFor='message' className='block font-medium mt-2.5 mb-2'>
               Message
               <textarea
                 id='message'
                 name='message'
-                placeholder=''
+                required
                 className='w-full p-2.5 rounded-xs border border-[#f5bcbc] outline-none mb-2.5 text-sm h-24 resize-none'
               ></textarea>
             </label>
@@ -59,7 +72,7 @@ const Contact = () => {
               Send Message
             </button>
 
-            <div id='result'></div>
+            <p className='mt-2'>{result}</p>
           </form>
         </div>
         <div className='p-5 border rounded-xs border-[#f5bcbc]  text-amber-50'>
@@ -71,7 +84,7 @@ const Contact = () => {
               target='_blank'
               className='bg-primary'
             >
-              yogesh-dhuri
+              /yogesh-dhuri
             </a>
           </div>
           <div className='p-3.5 rounded-xs mb-3.5 text-md bg-primary'>
