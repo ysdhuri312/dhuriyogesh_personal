6039ce47e158606e601ca0b91b99bb9199315e24
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-01-09 15:18:29 +0530

    Install vite-plugin-compression for increase build performance

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+    }),
+  ],
 });
